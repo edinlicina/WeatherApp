b7f4028c86c4cf55f2dd55b99674d541cb84b488
Edin Licina <edin.licina.2@gmail.com>
2025-10-02 21:51:35 +0200

    added radio button to settingsDialog

--- app/src/main/java/com/example/weatherapp/ui/home/SettingsDialog.kt
+++ app/src/main/java/com/example/weatherapp/ui/home/SettingsDialog.kt
@@ -1,14 +1,19 @@
 package com.example.weatherapp.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -29,10 +35,18 @@ import com.example.weatherapp.GeoLocationViewModel
 @Composable
 fun SettingsDialog(
     onDismissRequest: () -> Unit,
-    geoLocationVm: GeoLocationViewModel = viewModel(factory = GeoLocationViewModel.Factory)
+    geoLocationVm: GeoLocationViewModel = viewModel(factory = GeoLocationViewModel.Factory),
+    modifier: Modifier = Modifier
 ) {
     val settings by geoLocationVm.state.collectAsState()
     var location by remember { mutableStateOf("") }
+
+
+    val units = listOf("Metric", "Imperial", "Default")
+    val (selectedUnit, onUnitSelected) = remember { mutableStateOf(units[0]) }
+
+
+
 
     LaunchedEffect(settings) {
         settings?.let {
@@ -44,7 +58,7 @@ fun SettingsDialog(
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp),
+                .height(365.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
             Column(
@@ -59,12 +73,40 @@ fun SettingsDialog(
                             .wrapContentSize(Alignment.Center),
                         textAlign = TextAlign.Center,
                     )
-                    TextField(
-                        value = location,
-                        onValueChange = { value ->
-                            location = value
+
+                }
+                TextField(
+                    value = location,
+                    onValueChange = { value ->
+                        location = value
+                    }
+
+                )
+                Column(modifier.selectableGroup()) {
+                    units.forEach { text ->
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(56.dp)
+                                .selectable(
+                                    selected = (text == selectedUnit),
+                                    onClick = { onUnitSelected(text) },
+                                    role = Role.RadioButton
+                                )
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = (text == selectedUnit),
+                                onClick = null
+                            )
+                            Text(
+                                text = text,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(start = 16.dp)
+                            )
                         }
-                    )
+                    }
                     Button(onClick = {
                         geoLocationVm.fetch(location)
                         onDismissRequest()
@@ -72,7 +114,6 @@ fun SettingsDialog(
                         Text("Save")
                     }
                 }
-
             }
         }
     }
